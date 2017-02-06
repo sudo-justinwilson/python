1911c0403e23f0b4e6bf76b6fb3b983d43533fdb
sudo-justinwilson <sudo.justin.wilson@gmail.com>
2017-02-06 17:28:39 +1100

    do some exercises

--- python_data_structures_and_algorithms/chapter6/exercises.py
+++ python_data_structures_and_algorithms/chapter6/exercises.py
@@ -4,4 +4,26 @@
 
     A) 5
 
+R-6.2 Suppose an initially empty stack S has executed a total of 25 push operations, 12 top operations, and 10 pop operations, 3 of which raised Empty errors that were caught and ignored. What is the current size of S?
 
+	A) 18?
+
+R-6.3 Implement a function with signature transfer(S, T) that transfers all elements from stack S onto stack T, so that the element that starts at the top of S is the first to be inserted onto T, and the element at the bottom of S ends up at the top of T.
+
+	A)
+    def transfer(S, T):
+        while len(l) > 0:
+           s.append(l.pop())
+
+-6.4 Give a recursive method for removing all the elements from a stack.
+
+    A) 
+	def remove(obj):
+    if len(obj) > 0:
+        obj.pop()
+        return remove(obj)
+
+R-6.5 Implement a function that reverses a list of elements by pushing them onto
+a stack in one order, and writing them back to the list in reversed order.
+
+	A)
