b9c9107eb2a5e9833b1a4ea8c5989eec15c5b6eb
sudo-justinwilson <sudo.justin.wilson@gmail.com>
2017-02-06 17:16:29 +1100

    add chapter 6 exercises

---
+++ python_data_structures_and_algorithms/chapter6/exercises.py
@@ -0,0 +1,7 @@
+# exercises from chapter 6
+
+ R-6.1 What values are returned during the following series of stack operations, if executed upon an initially empty stack? push(5), push(3), pop(), push(2), push(8), pop(), pop(), push(9), push(1), pop(), push(7), push(6), pop(), pop(), push(4), pop(), pop().
+
+    A) 5
+
+
